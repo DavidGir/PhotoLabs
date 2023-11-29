dd36d4b9fae17f7933f020c029c65eafc637d507
David Giroux <DavidGiroux149@gmail.com>
2023-11-29 02:29:06 -0500

    add handler for click event and track state

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -3,14 +3,24 @@ import React, { useCallback, useState } from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+const PhotoFavButton = () => {
+  // State to track whether the photo is liked or not:
+  const [isLiked, setIsLiked] = useState(false);
+
+  // Make handler for the click event:
+  const clickLike = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <button onClick={clickLike}>
+          <FavIcon selected={isLiked} />
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default PhotoFavButton;
