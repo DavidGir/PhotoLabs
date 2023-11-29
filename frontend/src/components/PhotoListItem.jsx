1096214d658011144ca7e47409a59f3d7a8b3561
David Giroux <DavidGiroux149@gmail.com>
2023-11-29 01:36:04 -0500

    add props and JSX to PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,27 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  return (
+    <div className="photo-list__item" id={props.id}>
+      <img
+        src={props.imageSource}
+        alt={`Photo by ${props.username}`}
+        className="photo-list__image"
+      />
+      <div className="photo-list__user-details">
+        <img
+          src={props.profile}
+          alt={`${props.username}'s profile`}
+          className="photo-list__user-profile"
+        />
+        <div className="photo__list user-info">
+          <h3>{props.username}</h3>
+          <p className="photo-list__user-location">{`${props.location.city}, ${props.location.country}`}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
