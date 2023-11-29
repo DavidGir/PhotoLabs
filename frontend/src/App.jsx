01610a4c3402a94b0e5140e1f643cfb97516a253
David Giroux <DavidGiroux149@gmail.com>
2023-11-29 02:29:50 -0500

    import PhotoFavButton component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
+import PhotoFavButton from 'components/PhotoFavButton';
 import './App.scss';
 
 // Note: Rendering a single component to build components in isolation
