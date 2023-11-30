8893e68a86e560468422bc454dded04dc1850cf5
David Giroux <DavidGiroux149@gmail.com>
2023-11-30 15:37:37 -0500

    add JSX logic display count icon of fav pictures

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -3,10 +3,15 @@ import FavIcon from './FavIcon';
 
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ isFavPhotoExist }) => {
+const FavBadge = (props) => {
+  const { favorites } = props;
+  
   return (
     <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist}/>
+      <FavIcon displayAlert={favorites.length > 0}/>
+      {favorites.length > 0 && (
+        <span className="fav-badge__count">{favorites.length}</span>
+      )}
     </div>
   );
 };
