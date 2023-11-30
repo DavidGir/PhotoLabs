80ec383bd1d37b0fdafa472c663633bc1d247f96
David Giroux <DavidGiroux149@gmail.com>
2023-11-29 20:05:26 -0500

    pass in mock data to components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 import PhotoList from './components/PhotoList';
 import TopicList from './components/TopicList';
 import './App.scss';
-import TopNavigationBar from 'components/TopNavigationBar';
+import TopNavigationBar from './components/TopNavigationBar';
+import topics from './mocks/topics';
+import photos from './mocks/photos';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
 
   return (
     <div className="App">
-      <PhotoList />
-      <TopicList />
-      <TopNavigationBar />
+      <TopNavigationBar topics={topics}/>
+      <PhotoList photos={photos} />
+      {/* <TopicList topics={topics} /> */}
     </div>
   );
 };
