46b02674e1d1c22e2eae7a5251d91f735ec170b7
David Giroux <DavidGiroux149@gmail.com>
2023-11-29 20:04:15 -0500

    add topics mock data as props to component

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,32 +2,16 @@ import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
-const TopicList = () => {
+const TopicList = (props) => {
+  const { topics } = props;
+ 
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map(topicItem => (
+      {topics.map(topicItem => (
         <TopicListItem
           key={topicItem.id}
-          id={topicItem.id}
-          slug={topicItem.slug}
+          // id={topicItem.id}
+          // slug={topicItem.slug}
           title={topicItem.title}
         />
       ))}
