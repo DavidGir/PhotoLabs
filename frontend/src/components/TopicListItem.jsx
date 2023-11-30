bc58cf85c693bf013045be447e40c5f0465283a8
David Giroux <DavidGiroux149@gmail.com>
2023-11-29 20:03:47 -0500

    comment out JSX to show title as intended display

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import "../styles/TopicListItem.scss";
 
 const TopicListItem = (props) => {
-  console.log("props", props);
+  
   // Extract the properties from props:
   const { id, slug, title } = props;
   
   return (
     <div className="topic-list__item">
-      {/* <span>{id}</span> */}
-      {/* <span>{slug}</span> */}
+      <span>{id}</span>
+      <span>{slug}</span>
       <span>{title}</span>
     </div>
   );
