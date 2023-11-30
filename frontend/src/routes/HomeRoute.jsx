296a088a54b87a6fe70d32e9d014fa024faf619c
David Giroux <DavidGiroux149@gmail.com>
2023-11-30 15:31:37 -0500

    add "global state" to favorite photos

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
-import TopNavigationBar from './components/TopNavigationBar';
+import React, { useState } from 'react';
+import TopNavigationBar from '../components/TopNavigationBar';
+
 import '../styles/HomeRoute.scss';
-import PhotoList from './components/PhotoList';
+import PhotoList from '../components/PhotoList';
+import topics from '../mocks/topics';
+import photos from '../mocks/photos';
 
 const HomeRoute = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  const addFavorite = (photo) => {
+    setFavorites(prevFavorites => [...prevFavorites, photo]);
+  };
+
+  const removeFavorite = (photoId) => {
+    setFavorites(prevFavorites => prevFavorites.filter(photo => photo.id !== photoId));
+  };
+  
   return (
     <div className="home-route">
-      <TopNavigationBar />
-      <PhotoList />
+      <TopNavigationBar
+        favorites={favorites}
+        topics={topics}
+      />
+      <PhotoList
+        photos={photos}
+        addFavorite={addFavorite}
+        removeFavorite={removeFavorite}
+        favorites={favorites}
+      />
     </div>
   );
 };
