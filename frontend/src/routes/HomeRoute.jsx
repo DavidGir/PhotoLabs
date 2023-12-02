da40f15627d745ae525633c85ac59d3694dce771
David Giroux <DavidGiroux149@gmail.com>
2023-12-01 19:03:44 -0500

    refactor state management & separation of concerns

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,53 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
+import useApplicationData from '../hooks/useApplicationData';
 import TopNavigationBar from '../components/TopNavigationBar';
-
 import '../styles/HomeRoute.scss';
 import PhotoList from '../components/PhotoList';
 import topics from '../mocks/topics';
-import photos from '../mocks/photos';
 import PhotoDetailsModal from './PhotoDetailsModal';
 
 const HomeRoute = () => {
-  const [favorites, setFavorites] = useState([]);
-  // Add state to track currently selected photo for modal:
-  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const { state, actions } = useApplicationData();
+  const { photosWithFavoritedStatus, selectedPhoto, similarPhotos } = state;
+  const { addFavorite, removeFavorite, openModal, closeModal, isPhotoFavorited } = actions;
 
-  const [similarPhotos, setSimilarPhotos] = useState([]);
-
-  const addFavorite = (newFavorite) => {
-    setFavorites(prevFavorites => [...prevFavorites, newFavorite]);
-  };
-
-  const removeFavorite = (photoId) => {
-    setFavorites(prevFavorites => prevFavorites.filter(photo => photo.id !== photoId));
-  };
-
-  // Function to check if a photo is favorited:
-  const isPhotoFavorited = (photoId) => favorites.some(favPhoto => favPhoto.id === photoId);
-
-  // Add isFavorited property to each photo:
-  const photosWithFavoritedStatus = photos.map(photo => ({
-    ...photo,
-    isFavorited: isPhotoFavorited(photo.id)
-  }));
-  
-  const openModal = (selectedPhoto) => {
-    setSelectedPhoto(selectedPhoto);
-
-    // Extract similar photo objects from the selected photo's similarPhotos and add isFavorited property to them:
-    const similarPhotoObjects = Object.values(selectedPhoto.similarPhotos).map(photo => ({
-      ...photo,
-      isFavorited: isPhotoFavorited(photo.id)
-    }));
-    setSimilarPhotos(similarPhotoObjects);
-  };
-  
   return (
     <div className="home-route">
-      <TopNavigationBar
-        favorites={favorites}
-        topics={topics}
-      />
+      <TopNavigationBar favorites={state.favorites} topics={topics} />
       <PhotoList
         photos={photosWithFavoritedStatus}
         addFavorite={addFavorite}
@@ -58,7 +24,7 @@ const HomeRoute = () => {
       {selectedPhoto && (
         <PhotoDetailsModal
           photo={selectedPhoto}
-          closeModal={() => setSelectedPhoto(null)}
+          closeModal={closeModal}
           similarPhotos={similarPhotos}
           addFavorite={addFavorite}
           removeFavorite={removeFavorite}
