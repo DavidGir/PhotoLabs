efa28d158e160966bd3699020283708fd77419ac
David Giroux <DavidGiroux149@gmail.com>
2023-12-01 19:00:15 -0500

    minor comment change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import HomeRoute from './routes/HomeRoute';
 import './App.scss';
 
-// Note: Rendering a single component to build components in isolation
 const App = () => {
 
   return (
