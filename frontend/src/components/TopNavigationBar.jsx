78056d4e29e8b6aa064ce6f34091b0eaa8e1853f
David Giroux <DavidGiroux149@gmail.com>
2023-12-04 00:15:47 -0500

    pass click prop for topic

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -4,12 +4,12 @@ import TopicList from './TopicList';
 import '../styles/TopNavigationBar.scss';
 
 const TopNavigationBar = (props) => {
-  const { topics, favorites } = props;
+  const { topics, favorites, onTopicClick } = props;
   
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics}/>
+      <TopicList topics={topics} onTopicClick={onTopicClick}/>
       <FavBadge favorites={favorites}/>
     </div>
   );
