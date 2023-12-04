211a7748c9c7a23acc995329d3ae42c4abc26846
David Giroux <DavidGiroux149@gmail.com>
2023-12-04 02:02:11 -0500

    add dark mode toggle to icon and flashing state

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
 import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 import '../styles/TopNavigationBar.scss';
 
 const TopNavigationBar = (props) => {
-  const { topics, favorites, onTopicClick } = props;
+  const { topics, favorites, onTopicClick, onToggleDarkMode, isDarkMode } = props;
   
+  // Styling state to create flashing camera:
+  const [isFlashing, setIsFlashing] = useState(false);
+  // Handle click on camera to toggle dark mode and flash state:
+  const handleCameraClick = () => {
+    onToggleDarkMode();
+    setIsFlashing(true);
+    setTimeout(() => setIsFlashing(false), 1000);
+  };
+
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span className="top-nav-bar__logo">
+        PhotoLabs
+        <button onClick={handleCameraClick} className="icon-button">
+          <FontAwesomeIcon
+            icon={faCamera}
+            className={`icon ${isDarkMode ? 'icon-on' : 'icon-off'} ${isFlashing ? 'icon-flash' : ''}`}
+          />
+        </button>
+      </span>
       <TopicList topics={topics} onTopicClick={onTopicClick}/>
       <FavBadge favorites={favorites}/>
     </div>
