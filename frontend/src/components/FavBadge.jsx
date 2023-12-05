f5169158686979f85e6b9b371f59539653a99971
David Giroux <DavidGiroux149@gmail.com>
2023-12-04 22:59:57 -0500

    add onClick prop toggle to display all fav photos

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -4,14 +4,16 @@ import FavIcon from './FavIcon';
 import '../styles/FavBadge.scss';
 
 const FavBadge = (props) => {
-  const { favorites } = props;
-  
+  const { favorites, toggleFavoritesModal } = props;
+  console.log(favorites);
   return (
     <div className='fav-badge'>
-      <FavIcon displayAlert={favorites.length > 0}/>
-      {favorites.length > 0 && (
-        <span className="fav-badge__count">{favorites.length}</span>
-      )}
+      <button onClick={toggleFavoritesModal}>
+        <FavIcon displayAlert={favorites.length > 0}/>
+        {favorites.length > 0 && (
+          <span className="fav-badge__count">{favorites.length}</span>
+        )}
+      </button>
     </div>
   );
 };
